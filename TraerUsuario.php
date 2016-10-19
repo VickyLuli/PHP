a24704f19daa991460d65f30fd0d4363d1fb5312
VickyLuli <mvictoriadcl@gmail.com>
2016-10-19 17:03:31 -0300

    Update TraerUsuario.php

--- TraerUsuario.php
+++ TraerUsuario.php
@@ -1,32 +1,35 @@
 <?php
 $con=mysqli_connect('us-cdbr-azure-east-c.cloudapp.net','ba4f301b5a2fe0','77e42751','noctidb');
 
-if (mysqli_connect_errno()) {
-    printf("Error de conexión: %s\n", mysqli_connect_error());
-    exit();
-}
 
-$Email=$_GET["Email"];
-$query ="select * from Usuario WHERE Email='$Email'";
-$result = mysqli_query($con, $query);
-$objetos = array();
-while($row = mysqli_fetch_array($result)) 
-{ 
-	$IdUsuario=$row['IdUsuario'];
-	$Email=$row['Email'];
-	$Contrasena=$row['Contrasena'];
-	$Nombre=$row['Nombre'];
-	$Apellido=$row['Apellido'];
-	$FechadeNacimiento=$row['FechadeNacimiento'];
-	$Sexo=$row['Sexo'];
-	$Direccion=$row['Direccion'];
-	$Telefono=$row['Telefono'];
-	$DNI=$row['DNI'];
-	$objeto = array('IdUsuario'=> $IdUsuario, 'Email'=> $Email, 'Contrasena'=> $Contrasena, 'Nombre'=> $Nombre, 'Apellido'=> $Apellido, 'FechadeNacimiento'=> $FechadeNacimiento, 'Sexo'=> $Sexo, 'Direccion'=> $Direccion, 'Telefono'=> $Telefono, 'DNI'=> $DNI);	
-	array_push($objetos, $objeto);	
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: " .mysqli_connect_error();
 }
-$close = mysqli_close($con) 
-or die("Ha sucedido un error inesperado en la desconexion de la base de datos");
-header("Content-Type: application/json");
-$json_string = json_encode($objetos,JSON_UNESCAPED_UNICODE);
-echo $json_string;
+else
+{
+	$Email=$_GET["Email"];
+	$query ="SELECT * FROM Usuario WHERE Email='$email'";
+	$query_exc=mysqli_query($con, $query);
+	$objetos = array();
+	while($row=mysqli_fetch_assoc($query_exc)) 
+	{ 
+		$IdUsuario=$row['IdUsuario'];
+		$Email=$row['Email'];
+		$Contrasena=$row['Contrasena'];
+		$Nombre=$row['Nombre'];
+		$Apellido=$row['Apellido'];
+		$FechadeNacimiento=$row['FechadeNacimiento'];
+		$Sexo=$row['Sexo'];
+		$Direccion=$row['Direccion'];
+		$Telefono=$row['Telefono'];
+		$DNI=$row['DNI'];
+		$objeto = array('IdUsuario'=> $IdUsuario, 'Email'=> $Email, 'Contrasena'=> $Contrasena, 'Nombre'=> $Nombre, 'Apellido'=> $Apellido, 'FechadeNacimiento'=> $FechadeNacimiento, 'Sexo'=> $Sexo, 'Direccion'=> $Direccion, 'Telefono'=> $Telefono, 'DNI'=> $DNI);	
+		array_push($objetos, $objeto);	
+	}
+	header("Content-Type: application/json");
+	$json = json_encode($objetos, JSON_PRETTY_PRINT);
+	echo($json);
+}
+mysqli_close($con);
+?>
