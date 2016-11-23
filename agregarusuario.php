1f8003f623319d3700e87d39f68720042f680b0a
VickyLuli <mvictoriadcl@gmail.com>
2016-11-23 11:11:49 -0300

    Update agregarusuario.php

--- agregarusuario.php
+++ agregarusuario.php
@@ -5,10 +5,7 @@ if (mysqli_connect_errno()) {
     printf("Error de conexión: %s\n", mysqli_connect_error());
     exit();
 }		
-$string = file_get_contents('php://input');
-var_dump($string);
-$evento=json_decode($string,true);
-var_dump($evento);
+
 $query = "INSERT INTO Usuario (Email, Contrasena, Nombre, Apellido, FechadeNacimiento, Sexo, Direccion, Telefono, DNI) VALUES (?, ?, ?, ?,?,?,?,?,?)";
 $stmt=$con->prepare($query);
 $stmt->bind_param(
