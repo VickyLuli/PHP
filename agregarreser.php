b4fe176da6c74d99aa5ee92fa7ffe9c0993d9275
VickyLuli <mvictoriadcl@gmail.com>
2016-11-30 15:43:05 -0300

    Update agregarreser.php
    sdgsdfg

--- agregarreser.php
+++ agregarreser.php
@@ -9,6 +9,7 @@ if (mysqli_connect_errno()) {
 $string = file_get_contents("php://input");
 $evento=json_decode($string,true);
 $query = "INSERT INTO reserva (Nombre) VALUES (?)";
+echo $query;
 echo $evento["Nombre"];
 /*
 $stmt=$con->prepare($query);
